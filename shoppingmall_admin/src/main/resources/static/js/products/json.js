950b093e26bd3e4c6659d8f828fe6691b124c7d5
xodhksrjqnr <xodhksrjqnr@naver.com>
2025-02-03 21:52:52 +0900

    product json 정리

--- shoppingmall_admin/src/main/resources/static/js/products/json.js
+++ shoppingmall_admin/src/main/resources/static/js/products/json.js
@@ -1,167 +1,29 @@
 const inputJson = {
     objects : [
-        {
-            title : '기초상품명',
-            tag : 'input',
-            attributes : {
-                id : 'name',
-                name : 'name',
-                required : ''
-            }
-        },
-        {
-            title : '기초상품명(영문)',
-            tag : 'input',
-            attributes : {
-                id : 'nameEng',
-                name : 'nameEng',
-                required : ''
-            }
-        },
-        {
-            title : '기초상품코드',
-            tag : 'select',
-            attributes : {
-                id : 'code',
-                name : 'code',
-                required : ''
-            }
-        },
-        {
-            title : '기초상품코드id',
-            tag : 'select',
-            attributes : {
-                id : 'codeId',
-                name : 'codeId',
-                required : '',
-                hidden : ''
-            }
-        },
-        {
-            title : '판매가',
-            tag : 'input',
-            attributes : {
-                id : 'sellingPrice',
-                name : 'sellingPrice',
-                required : ''
-            }
-        },
-        {
-            title : '원가',
-            tag : 'input',
-            attributes : {
-                id : 'costPrice',
-                name : 'costPrice',
-                required : ''
-            }
-        },
-        {
-            title : '재고',
-            tag : 'input',
-            attributes : {
-                id : 'stock',
-                name : 'stock',
-                required : ''
-            }
-        },
-        {
-            title : '브랜드',
-            tag : 'input',
-            attributes : {
-                id : 'brand',
-                name : 'brand',
-                required : ''
-            }
-        },
-        {
-            title : '원산지',
-            tag : 'input',
-            attributes : {
-                id : 'placeOfOrigin',
-                name : 'placeOfOrigin',
-                required : ''
-            }
-        },
-        {
-            title : '상품 이미지',
-            tag : 'input',
-            attributes : {
-                type : 'file',
-                id : 'images',
-                name : 'images',
-                accept : 'image/*',
-                multiple : '',
-                required : ''
-            }
-        },
+        {title : '기초상품명', tag : 'input', attributes : {id : 'name', name : 'name', required : ''}},
+        {title : '기초상품명(영문)', tag : 'input', attributes : {id : 'nameEng', name : 'nameEng', required : ''}},
+        {title : '기초상품코드', tag : 'select', attributes : {id : 'code', name : 'code', required : ''}},
+        {title : '기초상품코드id', tag : 'select', attributes : {id : 'codeId', name : 'codeId', required : '', hidden : ''}},
+        {title : '판매가', tag : 'input', attributes : {id : 'sellingPrice', name : 'sellingPrice', required : ''}},
+        {title : '원가', tag : 'input', attributes : {id : 'costPrice', name : 'costPrice', required : ''}},
+        {title : '재고', tag : 'input', attributes : {id : 'stock', name : 'stock', required : ''}},
+        {title : '브랜드', tag : 'input', attributes : {id : 'brand', name : 'brand', required : ''}},
+        {title : '원산지', tag : 'input', attributes : {id : 'placeOfOrigin', name : 'placeOfOrigin', required : ''}},
+        {title : '상품 이미지', tag : 'input', attributes : {type : 'file', id : 'images', name : 'images', accept : 'image/*', multiple : '', required : ''}},
     ]
 };
 
 const viewJson = {
     objects : [
-        {
-            title : '기초상품명',
-            tag : 'span',
-            attributes : {
-                id : 'name'
-            }
-        },
-        {
-            title : '기초상품명(영문)',
-            tag : 'span',
-            attributes : {
-                id : 'nameEng'
-            }
-        },
-        {
-            title : '기초상품코드',
-            tag : 'span',
-            attributes : {
-                id : 'code'
-            }
-        },
-        {
-            title : '판매가',
-            tag : 'span',
-            attributes : {
-                id : 'sellingPrice'
-            }
-        },
-        {
-            title : '원가',
-            tag : 'span',
-            attributes : {
-                id : 'costPrice'
-            }
-        },
-        {
-            title : '재고',
-            tag : 'span',
-            attributes : {
-                id : 'stock'
-            }
-        },
-        {
-            title : '브랜드',
-            tag : 'span',
-            attributes : {
-                id : 'brand'
-            }
-        },
-        {
-            title : '원산지',
-            tag : 'span',
-            attributes : {
-                id : 'placeOfOrigin'
-            }
-        },
-        {
-            title : '상품 이미지',
-            tag : 'div',
-            attributes : {
-                id : 'image'
-            }
-        },
+        {title : '기초상품명', tag : 'span', attributes : {id : 'name'}},
+        {title : '기초상품명(영문)', tag : 'span', attributes : {id : 'nameEng'}},
+        {title : '기초상품코드', tag : 'span', attributes : {id : 'code'}},
+        {title : '판매가', tag : 'span', attributes : {id : 'sellingPrice'}},
+        {title : '원가', tag : 'span', attributes : {id : 'costPrice'}},
+        {title : '재고', tag : 'span', attributes : {id : 'stock'}},
+        {title : '브랜드', tag : 'span', attributes : {id : 'brand'}},
+        {title : '원산지', tag : 'span', attributes : {id : 'placeOfOrigin'}},
+        {title : '상품 이미지', tag : 'div', attributes : {id : 'image'}},
     ]
 };
 
@@ -200,44 +62,14 @@ const listJson = {
 };
 
 const listTUIJson = [
-    {
-        header: 'Id',
-        name: 'id'
-    },
-    {
-        header: 'Name',
-        name: 'name'
-    },
-    {
-        header: 'NameEng',
-        name: 'nameEng'
-    },
-    {
-        header: 'Code',
-        name: 'code'
-    },
-    {
-        header: 'SellingPrice',
-        name: 'sellingPrice'
-    },
-    {
-        header: 'CostPrice',
-        name: 'costPrice'
-    },
-    {
-        header: 'Stock',
-        name: 'stock'
-    },
-    {
-        header: 'Brand',
-        name: 'brand'
-    },
-    {
-        header: 'PlaceOfOrigin',
-        name: 'placeOfOrigin'
-    },
-    {
-        header: 'CreatedDate',
-        name: 'createdDate'
-    }
+    {header: 'Id', name: 'id'},
+    {header: 'Name', name: 'name'},
+    {header: 'NameEng', name: 'nameEng'},
+    {header: 'Code', name: 'code'},
+    {header: 'SellingPrice', name: 'sellingPrice'},
+    {header: 'CostPrice', name: 'costPrice'},
+    {header: 'Stock', name: 'stock'},
+    {header: 'Brand', name: 'brand'},
+    {header: 'PlaceOfOrigin', name: 'placeOfOrigin'},
+    {header: 'CreatedDate', name: 'createdDate'}
 ]
