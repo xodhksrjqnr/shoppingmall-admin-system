f4acdfda6c4baa82910303c4acadb7aba4f28141
xodhksrjqnr <xodhksrjqnr@naver.com>
2025-02-05 00:08:12 +0900

    product 등록 및 조회 기능에 brand 로직 추가

--- shoppingmall_admin/src/main/resources/static/js/products/json.js
+++ shoppingmall_admin/src/main/resources/static/js/products/json.js
@@ -7,7 +7,7 @@ const inputJson = {
         {title : '판매가', tag : 'input', attributes : {id : 'sellingPrice', name : 'sellingPrice', required : ''}},
         {title : '원가', tag : 'input', attributes : {id : 'costPrice', name : 'costPrice', required : ''}},
         {title : '재고', tag : 'input', attributes : {id : 'stock', name : 'stock', required : ''}},
-        {title : '브랜드', tag : 'input', attributes : {id : 'brand', name : 'brand', required : ''}},
+        {title : '브랜드', tag : 'select', attributes : {id : 'brand', name : 'brand', required : ''}},
         {title : '원산지', tag : 'input', attributes : {id : 'placeOfOrigin', name : 'placeOfOrigin', required : ''}},
         {title : '상품 이미지', tag : 'input', attributes : {type : 'file', id : 'images', name : 'images', accept : 'image/*', multiple : '', required : ''}},
     ]
@@ -29,35 +29,9 @@ const viewJson = {
 
 const listJson = {
     objects : [
-        {
-            title : '상품명(한글/영문)',
-            tag : 'input',
-            attributes : {
-                name : 'name'
-            }
-        },
-        {
-            title : '기초상품코드',
-            tag : 'input',
-            attributes : {
-                name : 'code'
-            }
-        },
-        {
-            title : '브랜드',
-            tag : 'select',
-            elements : [
-                {tag : 'option', text : '', attributes : {value : ''}},
-                {tag : 'option', text : 'brand1', attributes : {value : 'brand1'}},
-                {tag : 'option', text : 'brand2', attributes : {value : 'brand2'}},
-                {tag : 'option', text : 'brand3', attributes : {value : 'brand3'}},
-                {tag : 'option', text : 'brand4', attributes : {value : 'brand4'}},
-                {tag : 'option', text : 'brand5', attributes : {value : 'brand5'}}
-            ],
-            attributes : {
-                name : 'brand',
-            }
-        }
+        {title : '상품명(한글/영문)', tag : 'input', attributes : {name : 'name'}},
+        {title : '기초상품코드', tag : 'input', attributes : {name : 'code'}},
+        {title : '브랜드', tag : 'div', attributes : {id : 'brand', name : 'brand'}}
     ]
 };
 
